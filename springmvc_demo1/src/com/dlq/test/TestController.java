a241728934c1307b643288423c70a2e6a1121283
D·Teach <1114626450@qq.com>
2020-07-29 23:40:02 +0800

    SpringMVC实例1

---
+++ springmvc_demo1/src/com/dlq/test/TestController.java
@@ -0,0 +1,19 @@
+package com.dlq.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {						
+	
+	/**						               					                   
+	 * 假设：localhost:8080/springmvc_demo1/hello
+	 * @return
+	 */
+	@RequestMapping("hello")
+	public String hello() {
+		System.out.println("SUCCESS");
+		return "success";
+	}
+
+}
