f3bb5c3d2107fb62f12ec49e6eed0105de83a2a3
D·Teach <1114626450@qq.com>
2020-07-30 22:47:11 +0800

    SpringMVC实例3

---
+++ springmvc_demo3/src/com/dlq/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.dlq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+	
+	/* View作用：处理模型数据，实现页面跳转（转发，重定向）
+	 * View类型：
+	 * InternalResourceView：转发视图
+	 * JstlView：转发视图
+	 * RedirectView：重定向视图
+	 */
+	@RequestMapping(value="/test",method=RequestMethod.POST)
+	public String test(String username) {
+//		return "redirect:/index.jsp";
+		System.out.println(username);
+		return "success";
+	}
+	
+}
