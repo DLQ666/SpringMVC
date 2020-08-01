78e398d162ac82eabdce06faa4fc1096f9286ad8
D·Teach <1114626450@qq.com>
2020-08-01 19:44:03 +0800

    Spring与SpringMVC整合实例

---
+++ SS/src/com/dlq/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.dlq.controller;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dlq.bean.Teacher;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/testListener")
+	private void testListener(HttpSession session) {
+		//获取spring所管理的teacher对象
+		ServletContext servletContext = session.getServletContext();
+		ApplicationContext ac = (ApplicationContext) servletContext.getAttribute("ac");
+		Teacher teacher = ac.getBean("teacher",Teacher.class);
+		System.out.println(teacher);
+	}
+}
