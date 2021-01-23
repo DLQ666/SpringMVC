38fcbd74ba2b7f583146d8c78c3434263baaba2f
D·Teach <1114626450@qq.com>
2021-01-23 16:54:06 +0800

    SpringMVC一些测试

--- springmvc_demo1/src/com/dlq/test/TestController.java
+++ springmvc_demo1/src/com/dlq/test/TestController.java
@@ -1,9 +1,15 @@
 package com.dlq.test;
 
+import com.dlq.bean.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Map;
+
+@RestController
 public class TestController {						
 	
 	/**						               					                   
@@ -11,9 +17,8 @@ public class TestController {
 	 * @return
 	 */
 	@RequestMapping("/hello")
-	public String hello() {
-		System.out.println("SUCCESS");
+	public String hello(Book book) {
+		System.out.println(book);
 		return "success";
 	}
-
 }
